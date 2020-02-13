64d77c1f68e81bd0fc9e0eafb668881da656f22c
argierdfj <argierincon@gmail.com>
2020-02-12 22:15:54 -0500

    evnto del boton y funcion isValid

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import validator from './validator.js';
 
 console.log(validator);
+const verifyCreditCard = document.getElementById('verifyCreditCard');
+
+verifyCreditCard.addEventListener('click', () => {
+    const creditCard = document.getElementById('creditCard')
+    const creditCardNumber = creditCard.value;
+    if (!creditCardNumber) {
+        alert('Debes ingresar tu número de tarjeta para continuar 😒');
+        return;
+    };
+    validator.isValid(creditCardNumber);
+});
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,17 @@
 const validator = {
-  isValid : () => {},
+  isValid : (creditCardNumber) => {
+    console.log(creditCardNumber);
+    const cardNumber = creditCardNumber.split('');
+    cardNumber.reverse();
+    console.log(cardNumber);
+    for (let i = 0; i < cardNumber.length; i++) {
+        console.log(cardNumber[i]);
+        if (i % 2 == 1) {
+            console.log(cardNumber[i] * 2);
+        }
+    }
+    return true;
+  },
   maskify : () => {}
 }
 
