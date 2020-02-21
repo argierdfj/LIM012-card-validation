841bfe13520b9a559803c2df00cf640881cc163d
argierdfj <argierincon@gmail.com>
2020-02-21 10:49:07 -0500

    identando index.js

--- src/index.js
+++ src/index.js
@@ -7,45 +7,45 @@ const messageScreen = document.getElementById('messageScreen');
 
 
 btnVerify.addEventListener('click', () => {
-    const creditCard = document.getElementById('creditCard')
-    const creditCardNumber = creditCard.value;
-    if (!creditCardNumber) {
-        alert('Debes ingresar el número de tarjeta para continuar');
-        return
-    } else if (creditCardNumber.length < 16) {
-        alert('Faltan números');
-        return;
-    }
+  const creditCard = document.getElementById('creditCard')
+  const creditCardNumber = creditCard.value;
+  if (!creditCardNumber) {
+    alert('Debes ingresar el número de tarjeta para continuar');
+    return
+  } else if (creditCardNumber.length < 16) {
+    alert('Faltan números');
+    return;
+  }
 
-    // mainScreen.style.display = 'none';
-    // messageScreen.style.display = 'block';
+  // mainScreen.style.display = 'none';
+  // messageScreen.style.display = 'block';
 
-    mainScreen.classList.add('hide');
-    messageScreen.classList.remove('hide')
+  mainScreen.classList.add('hide');
+  messageScreen.classList.remove('hide')
 
-    const cardValid = validator.isValid(creditCardNumber);
-    const hiddenCardNumber = validator.maskify(creditCardNumber);
+  const cardValid = validator.isValid(creditCardNumber);
+  const hiddenCardNumber = validator.maskify(creditCardNumber);
 
-    const hiddenCard = document.getElementById('hiddenCard');
-    const message = document.getElementById('message');
+  const hiddenCard = document.getElementById('hiddenCard');
+  const message = document.getElementById('message');
 
-    if (cardValid === true) {
-        hiddenCard.innerText = `${hiddenCardNumber}`;
-        message.innerText = `Es válido para realizar compras online ✔️`;
-        btnRestart.value = `Verificar otra TDC Virtual`;
-    } else {
-        hiddenCard.innerText = `${hiddenCardNumber}`;
-        message.innerText = `No es válido, por favor verifique. ❌`;
-        btnRestart.value = `Verificar nuevamente`;
-    }
+  if (cardValid === true) {
+    hiddenCard.innerText = `${hiddenCardNumber}`;
+    message.innerText = `Es válido para realizar compras online ✔️`;
+    btnRestart.value = `Verificar otra TDC Virtual`;
+  } else {
+    hiddenCard.innerText = `${hiddenCardNumber}`;
+    message.innerText = `No es válido, por favor verifique. ❌`;
+    btnRestart.value = `Verificar nuevamente`;
+  }
 });
 
 btnRestart.addEventListener('click', () => {
-    const mainScreen = document.getElementById('mainScreen');
-    const messageScreen = document.getElementById('messageScreen');
-    const creditCard = document.getElementById('creditCard');
+  const mainScreen = document.getElementById('mainScreen');
+  const messageScreen = document.getElementById('messageScreen');
+  const creditCard = document.getElementById('creditCard');
 
-    creditCard.value = '';
-    messageScreen.classList.add('hide');
-    mainScreen.classList.remove('hide');
+  creditCard.value = '';
+  messageScreen.classList.add('hide');
+  mainScreen.classList.remove('hide');
 });
